c3039d3d01b05e4bc64fedd578d99c5e40006590
vidya kalappurayil viswanathan <vk427@afsconnect2.njit.edu>
2017-09-26 21:11:44 -0400

    Added array functions

--- oop.php
+++ oop.php
@@ -32,65 +32,83 @@ $obj->printStrPos($myWord);
 $myMsg ="Welcome to WSD class";
 $obj->printStrReplace($myMsg);
 
+
+
+$color = array("red","green","blue","yellow");
+$fruit = array ("apple","avocado","berries","banana");
+$myColors = array("red","pink","orange","green","pink");
+
+$obj->printArrayChunk($color);
+$obj->printArrayCombine($color,$fruit);
+$obj->printArrayDiff($color,$myColors);
+$obj->printArrayPop($myColors);
+$obj->printArrayPush($myColors);
+$obj->printShuffle($myColors);
+$obj->printArraySort($color);
+$obj->printList($color);
+$obj->printArrayCount($fruit);
+$obj->printArraySearch($fruit);
+
+
 class main {
 	public function __construct() {
-		echo 'Hello, i\'m an Object </br>';
+		echo '<h2>String Functions</h2></br>';
 	}
 
 
  public function printchr($str) {
-      echo '<h1> Print chr function </h1>';
+      echo '<h4> Print chr function </h4>';
       print($str);
       echo '<hr>';
     }
 
 
 public function printecho($text) {
-      echo '<h1> Print echo function </h1>';
+      echo '<h4> Print echo function </h4>';
       echo "$text";
       echo '<hr>';
     }
 
 public function printexplode  ($txt) {
-      echo '<h1> Print explode function </h1>';
+      echo '<h4> Print explode function </h4>';
       print_r (explode(" ",$txt));
       echo '<hr>';
     }
 
 public function printcount($strArray)
-{     echo '<h1> Print count_char function</h1>';
+{     echo '<h4> Print count_char function</h4>';
       print_r (count_chars($strArray,1));
       echo '<hr>';
 }
 
 public function printhtmlEntities($myUrl)
-{     echo '<h1> Print html_entities function</h1>';
+{     echo '<h4> Print html_entities function</h4>';
       print_r (htmlentities($myUrl));
       echo '<hr>';
 }
 
 public function printHtmlDecode($myVar)
-{     echo '<h1> Print html_entity_decode function</h1>';
+{     echo '<h4> Print html_entity_decode function</h4>';
       echo html_entity_decode($myVar, ENT_QUOTES);
       echo '<hr>';
 }
 
 public function printLeftTrim($myStr)
-{     echo '<h1> Print Left Trim(ltrim) function</h1>';
+{     echo '<h4> Print Left Trim(ltrim) function</h4>';
       echo ltrim($myStr," Welcome to ");
       echo '<hr>';
 }
 
 
 public function printRightTrim($myStr)
-{     echo '<h1> Print Right Trim(rtrim) function</h1>';
+{     echo '<h4> Print Right Trim(rtrim) function</h4>';
       echo rtrim($myStr," php tutorial");
       echo '<hr>';
 }
 
 
 public function printStrLen($myWord)
-{     echo '<h1> Print strlen function</h1>';
+{     echo '<h4> Print strlen function</h4>';
       print strlen($myWord);
       echo '<hr>';
 }
@@ -98,23 +116,98 @@ public function printStrLen($myWord)
 
 
 public function printStrPos($myWord)
-{     echo '<h1> Print string position function</h1>';
+{     echo '<h4> Print string position function</h4>';
       print strpos($myWord, 'l');
       echo '<hr>';
 }
 
 public function printStrReplace($myMsg)
-{     echo '<h1> Print substring replace function</h1>';
+{     echo '<h4> Print substring replace function</h4>';
       print substr_replace($myMsg, "IS601" ,11);
       echo '<hr>';
 }
 
 
+#**********************************************************Array Functions*****************************************************************************************
 
-	public function __destruct() {
+    public function printArrayChunk($color){
+      echo '<h2>Array Functions</h2></br>';
+      echo '<h4> Array chunk function</h4>';
+      print_r (array_chunk($color,2));
+      echo '<hr>';
+    }
+
+
+    public function printArrayCombine($color,$fruit){
+      echo '<h4> Array combine function</h4>';
+      print_r (array_combine($color,$fruit));
+      echo '<hr>';
+    }
+
+    public function printArrayDiff($color,$myColors){
+      echo '<h4> Array Difference function</h4>';
+      print_r (array_diff($color,$myColors));
+      echo '<hr>';
+    }
+
+
+    public function printArrayPop($myColors){
+      echo '<h4> Array Pop function</h4>';
+      print_r(array_pop($myColors));
+      echo '<br>';
+      print_r($myColors);
+      echo '<hr>';
+    }
+
+    public function printArrayPush($myColors){
+      echo '<h4> Array Push function</h4>';
+      array_push($myColors,"white");
+      print_r($myColors);
+      echo '<hr>';
+    }
+
+
+    public function printShuffle($myColors){
+      echo '<h4> Array Shuffle function</h4>';
+      shuffle($myColors);
+      print_r ($myColors);
+      echo '<hr>';
+    }
+
+   public function printArraySort($color){
+      echo '<h4> Array Sort function</h4>';
+      sort($color);
+      print_r ($color);
+      echo '<hr>';
+    }
+
+    public function printList($color){
+      echo '<h4> Array list function</h4>';
+      list ($a,$b,$c,$d) = $color;
+      echo"I like the colors $a, $b, $c and $d";
+      echo '<hr>';
+    }
+
+    public function printArrayCount($fruit){
+      echo '<h4> Array Count function</h4>';
+      $numElements= (count($fruit));
+      echo "The array fruit has $numElements elements";
+      echo '<hr>';
+    }
+
+        public function printArraySearch($fruit){
+      echo '<h4> Array Search function</h4>';
+        echo array_search("banana",$fruit);
+      echo '<hr>';
+    }
+
+public function __destruct() {
 		echo 'I am done';
 
 	}
+
+
+
 
 }
 
